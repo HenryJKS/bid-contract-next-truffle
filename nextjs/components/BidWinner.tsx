fc575e0cfcd58244e6b3d8101cecfd4ca23776c9
Henry Jooji Kitadani Sato <henry.jooji.kitadan1@managementsolutions.com>
2024-10-17 09:37:55 -0300

    resolve: form

--- nextjs/components/BidWinner.tsx
+++ nextjs/components/BidWinner.tsx
@@ -35,7 +35,7 @@ export default function BidWinner() {
                 <div className="flex items-center gap-4">
                     <span className="hidden rounded-full bg-gray-100 p-2 text-gray-600 sm:block">
                         <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-width="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            <path stroke="currentColor" strokeWidth="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
 
                     </span>
--- nextjs/components/Withdraw.tsx
+++ nextjs/components/Withdraw.tsx
@@ -26,7 +26,7 @@ export default function Withdraw() {
             <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
                 <div className="flex space-x-8">
                     <div className="mx-auto max-w-lg">
-                        <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">Participate</h1>
+                        <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">Check Winner</h1>
 
                         <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
                             Win of the NFTs by placing a bid. The highest bidder will be the winner.
